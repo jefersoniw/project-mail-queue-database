22b28a3c4dfccb80b4741761c5ad8fb59aa756ef
Jeferson Chagas Silva <jeferson.chagas@salvador.ba.gov.br>
2024-03-26 15:36:13 -0300

    feat: Validação form request criada.

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function __invoke(RegisterRequest $request)
+    {
+    }
+}
