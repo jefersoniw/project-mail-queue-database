3a85f537299efc68262a4b0f9476ac63c6711670
Jeferson Chagas Silva <jeferson.chagas@salvador.ba.gov.br>
2024-03-26 16:38:34 -0300

    feat: criando usuário.

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
     public function __invoke(RegisterRequest $request)
     {
+        $input = $request->validated();
+        $user = User::query()->create($input);
+
+        return \response()->json($user);
     }
 }
